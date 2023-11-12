36dc72f6a7d6b7033aa276a359a76b839cd79c3a
Christopher Goodwin <54195497+cgoodwinDH@users.noreply.github.com>
2023-11-12 00:46:42 -0600

    Added additional file cleaning source files

---
+++ src/FileCleaningScripts/MeldungenüberdieEntwicklunginderöffentlichenMeinungsbildungPDFConversion.py
@@ -0,0 +1,22 @@
+##########################################################################
+# Author: Christopher Thomas Goodwin
+# Creation Date: 2023.11.12
+# Summary: Converts all files from Meldungen über die Entwicklung in der öffentlichen Meinungsbildung
+# from the NSHWE De Gruyter database from HTML to PDF format
+#
+# This script simply gets all the file names in the path using GLOB
+# then it goes through using PDFKit which calls WKHTMLTOPDF
+##########################################################################
+
+import pdfkit
+import glob
+
+# My path to the database files
+path = "/Users/cgoodwin/Desktop/Meldungen über die Entwicklung in der öffentlichen Meinungsbildung/*.html"
+
+# Get all files in the directory into a list
+files = glob.glob(path) #*.html above ensures only HTML files are collected
+
+for file in files:
+    # from_file is for local files and local file access MUST be enabled
+    pdfkit.from_file(file, file[0:len(file)-5] + ".pdf", options={"enable-local-file-access" :""})
